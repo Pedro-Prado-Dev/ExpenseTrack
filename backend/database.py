a3f0f37de4bb1069e4f63c7a11fc4857b7ab8bcd
Pedro-Prado-Dev <pedroppaiva1@hotmail.com>
2024-05-13 14:27:27 -0300

    :feat: feat: conection with bd

--- backend/database.py
+++ backend/database.py
@@ -1,30 +1,8 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from datetime import datetime
 
-
-Base = declarative_base()
-class Gasto(Base):
-    __tablename__ = 'gastos'
-
-    id = Column(Integer, primary_key=True)
-    descricao = Column(String)
-    valor = Column(Float)
-    data = Column(DateTime, default=datetime.now)
-
-    def __repr__(self):
-        return f"<Gasto(id={self.id}, descricao={self.descricao}, valor={self.valor}, data={self.data})>"
-    
-# Configuração do banco de dados
-DATABASE_URL = 'postgresql://<user>:<password>@<host>:<port>/<database>'
+DATABASE_URL = 'postgresql://root:root@database:5432/expensetrack'
 engine = create_engine(DATABASE_URL)
 
-# Crie as tabelas no banco de dados
-Base.metadata.create_all(engine)
-
-# Crie uma sessão para interagir com o banco de dados
 Session = sessionmaker(bind=engine)
 session = Session()
-
