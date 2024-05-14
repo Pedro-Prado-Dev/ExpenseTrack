f8359244d693706782c26f6867a1fdc3de31929b
Pedro-Prado-Dev <pedroppaiva1@hotmail.com>
2024-05-14 01:25:59 -0300

    :recycle: refactor: update and patch CRUD

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import psycopg2
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from models import Gasto
@@ -26,7 +27,7 @@ def adicionar_gasto():
         novo_gasto = Gasto(descricao=descricao, valor=valor)
         session.add(novo_gasto)
         session.commit()
-        return jsonify({"message": "Gasto adicionado com sucesso"}), 201
+        return jsonify({"message": "Gasto adicionado com sucesso", "gasto_id": novo_gasto.gasto_id}), 201
     except Exception as e:
         session.rollback()
         return jsonify({"error": str(e)}), 500
@@ -34,11 +35,21 @@ def adicionar_gasto():
 @app.route('/gastos', methods=['GET'])
 def listar_gastos():
     gastos = session.query(Gasto).all()
-    return jsonify([gasto.__dict__ for gasto in gastos])
+    gastos_serializados = []
+    for gasto in gastos:
+        gasto_serializado = {
+            'gasto_id': gasto.gasto_id,
+            'descricao': gasto.descricao,
+            'valor': gasto.valor,
+            'data': gasto.data.isoformat()  # Converta a data para uma string ISO
+        }
+        gastos_serializados.append(gasto_serializado)
+    return jsonify(gastos_serializados)
+
 
 @app.route('/gastos/<int:gasto_id>', methods=['PUT'])
 def atualizar_gasto(gasto_id):
-    gasto = session.query(Gasto).filter_by(id=gasto_id).first()
+    gasto = session.query(Gasto).filter_by(gasto_id=gasto_id).first()
     if not gasto:
         return jsonify({"error": "Gasto não encontrado"}), 404
 
@@ -53,21 +64,21 @@ def atualizar_gasto(gasto_id):
 
     try:
         session.commit()
-        return jsonify({"message": "Gasto atualizado com sucesso"})
+        return jsonify({"message": "Gasto atualizado com sucesso", "gasto_id": gasto.gasto_id})
     except Exception as e:
         session.rollback()
         return jsonify({"error": str(e)}), 500
 
 @app.route('/gastos/<int:gasto_id>', methods=['DELETE'])
 def excluir_gasto(gasto_id):
-    gasto = session.query(Gasto).filter_by(id=gasto_id).first()
+    gasto = session.query(Gasto).filter_by(gasto_id=gasto_id).first()
     if not gasto:
         return jsonify({"error": "Gasto não encontrado"}), 404
 
     try:
         session.delete(gasto)
         session.commit()
-        return jsonify({"message": "Gasto excluído com sucesso"})
+        return jsonify({"message": "Gasto excluído com sucesso"}), 200
     except Exception as e:
         session.rollback()
         return jsonify({"error": str(e)}), 500
