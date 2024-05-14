02e6ae688a6f0358661e8f5680e96f48cbe9481a
Pedro-Prado-Dev <pedroppaiva1@hotmail.com>
2024-05-14 01:26:26 -0300

    :recycle: refactor: update database

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from models import Base
 
-DATABASE_URL = 'postgresql://root:root@database:5432/expensetrack'
+DATABASE_URL = 'postgresql://root:root@database:5432/expensetrack?client_encoding=utf8'
 engine = create_engine(DATABASE_URL)
 
 Session = sessionmaker(bind=engine)
 session = Session()
+
+Base.metadata.create_all(engine)  # Esta linha cria as tabelas no banco de dados
